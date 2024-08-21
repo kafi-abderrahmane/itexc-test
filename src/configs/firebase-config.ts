ba95d23b5df33f2a4b7afecca6eda93fdbb11a0a
kafi-abderrahmane <kafi.abderrahmane.ka@gmail.com>
2024-08-21 14:12:48 +0100

    chore: add Firebase configuration

---
+++ src/configs/firebase-config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "***REMOVED***",
+  authDomain: "itexc-b91ff.firebaseapp.com",
+  projectId: "itexc-b91ff",
+  storageBucket: "itexc-b91ff.appspot.com",
+  messagingSenderId: "871112777390",
+  appId: "1:871112777390:web:16c434a46f17fb5c1c173f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
