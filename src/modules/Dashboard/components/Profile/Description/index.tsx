7bedbe650f032b146b3cde2858fe492ba2462cd2
kafi-abderrahmane <kafi.abderrahmane.ka@gmail.com>
2024-08-23 17:43:15 +0100

    feat(profile) : add profile desciption

--- src/modules/Dashboard/components/Profile/Description/index.tsx
+++ src/modules/Dashboard/components/Profile/Description/index.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 
-import { useNavigate } from "react-router-dom";
-
-import locationIcon from "@/assets/icon/Location.svg";
-import editIcon from "@/assets/icon/Edit.svg";
-
-import coverP from "@/assets/images/cover.png";
-import profileP from "@/assets/images/profile.png";
 import "./description.scss";
 
 const ProfileDescription: React.FC = () => {
-  const navigate = useNavigate();
   return (
     <div className="profile-Description-display">
       <h1>Profile Description</h1>
